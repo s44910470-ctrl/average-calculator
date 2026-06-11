838d92a897f9621938ae97def463047282247e4c
s44910470-ctrl <s44910470@gmail.com>
2026-06-11 15:18:01 +0530

    Add provider architecture

--- quantumtrade/app/layout.tsx
+++ quantumtrade/app/layout.tsx
@@ -4,11 +4,16 @@ export const metadata = {
 };
 
 import './globals.css';
+import { QueryProvider } from '@/components/providers/query-provider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <QueryProvider>
+          {children}
+        </QueryProvider>
+      </body>
     </html>
   );
 }
